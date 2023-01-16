2e3cf2c60cadb194e9a19170f8a0ea11136caf8c
Sanskriti Gupta <21sanskritigupta@gmail.com>
2023-01-16 22:36:58 +0530

    added new header and set header functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,16 @@ import React, { useState } from "react";
 
 // Header Component
 const Header = () => {
+  // let new_title = "hello foods"
+  const [new_title, setNew_title ] = useState("hello world")
+
+  // each time the dom refreshesh component will re-render
+  console.log("render")
+
     return (
       <div className="header">
         <h1 className="logo">FoodApp</h1>
+        <button onClick={()=>{setNew_title("food world") }}>{new_title}</button>
         <nav className="nav-bar">
           <ul>
             <li>
