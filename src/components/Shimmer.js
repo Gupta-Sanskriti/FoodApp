f825cd8eb20fcde69e413027b89faacae916de9f
Sanskriti Gupta <21sanskritigupta@gmail.com>
2023-01-19 09:22:42 +0530

    Added shimmer functional component

---
+++ src/components/Shimmer.js
@@ -0,0 +1,13 @@
+const Shimmer = () =>{
+    return(
+        <div className="Shimmer_card">
+            <div className="Shimmer_card_item">
+                <div className="Shimmer_card_image"></div>
+                <h1 className="Shimmer_card_heading"></h1>
+                <p className="Shimmer_card_para"></p>
+            </div>
+        </div>
+    )
+}
+
+export default Shimmer;
