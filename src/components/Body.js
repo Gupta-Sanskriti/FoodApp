c2360b5f1944c9d89d62dd3d1b71b8ee82a1e61d
Sanskriti Gupta <21sanskritigupta@gmail.com>
2023-01-19 09:49:34 +0530

    added shimmer into body with conditional operator and style a lil bit

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,33 +15,46 @@ const Body = () => {
   //   console.log("render Effect");
   // },[restaurants])
 
-  useEffect(()=>{
-    getRestaurants()
-  },[])
+  useEffect(() => {
+    getRestaurants();
+  }, []);
 
-  async function getRestaurants(){
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9161991&lng=80.9442732&page_type=DESKTOP_WEB_LISTING");
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9161991&lng=80.9442732&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
     console.log(json);
-    setRestaurant(json?.data?.cards[2]?.data?.data?.cards)
+    setRestaurant(json?.data?.cards[2]?.data?.data?.cards);
   }
 
   return (
-    <Shimmer/>
-    // <div className="body-container">
-    //   <div className="search-field">
-    //     <Search searchText={searchText} restaurant={restaurants} setSearchText={setSearchText} setRestaurant={setRestaurant}/>
-    //   </div>
-    //   <div className="restaurant">
-    //     <h1 className="card-head">Restaurants</h1>
-    //     <div className="cards">
-    //       {restaurants.map((restaurant) => (
-    //         <CardsComponent restaurant={restaurant} key={restaurant.data.id} />
-    //       ))}
-    //       {/* <CardsComponent resturant={restaurantList[0]} /> */}
-    //     </div>
-    //   </div>
-    // </div>
+    <div className="body-container">
+      <div className="search-field">
+        <Search
+          searchText={searchText}
+          restaurant={restaurants}
+          setSearchText={setSearchText}
+          setRestaurant={setRestaurant}
+        />
+      </div>
+      {restaurants.length === 0 ? (
+        <Shimmer />
+      ) : (
+        <div className="restaurant">
+          <h1 className="card-head">Restaurants</h1>
+          <div className="cards">
+            {restaurants.map((restaurant) => (
+              <CardsComponent
+                restaurant={restaurant}
+                key={restaurant.data.id}
+              />
+            ))}
+            {/* <CardsComponent resturant={restaurantList[0]} /> */}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,11 +3,11 @@ import Search from "./Search";
 const Shimmer = () =>{
     return(
         <>
-        <Search/>
+            <h1 className="Shimmer_rest_heading"></h1>
             <div className="Shimmer_card">
                 <div className="Shimmer_card_item">
                     <div className="Shimmer_card_image"></div>
-                    <h1 className="Shimmer_card_heading"></h1>
+                    <h1 className="Shimmer_card_heading">Loading page..</h1>
                     <p className="Shimmer_card_para"></p>
                 </div>
             </div>
