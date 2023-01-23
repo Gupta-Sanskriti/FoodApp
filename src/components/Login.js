3ffb22ab960bbfcda1e91e4d94ae0962d903cf35
Sanskriti Gupta <21sanskritigupta@gmail.com>
2023-01-23 22:54:16 +0530

    redesigned with formik and styled it with css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,91 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React from "react";
+import { useFormik } from "formik";
 
-const Login = () =>{
-    return(
-        <>
-            <div className="login-card">
-                <h1>Login in our FoodVilla app</h1>
-                <Formik
-       initialValues={{ email: '', password: '' }}
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2));
-           setSubmitting(false);
-         }, 400);
-       }}
-     >
-       {({ isSubmitting }) => (
-         <Form>
-           <Field type="email" name="email" />
-           <ErrorMessage name="email" component="div" />
-           <Field type="password" name="password" />
-           <ErrorMessage name="password" component="div" />
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </Form>
-       )}
-     </Formik>
-            </div>
-        </>
-    )
-}
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log("form-data", values);
+    },
+    validate: (values) => {
+      // values.name, values.email, values.password
+      // handle error: error.name, error.email, error.password
+      // error.name = "this field is required"
+      let errors = {};
+
+      if (!values.name) {
+        errors.name = "Required!";
+      }
+      if (!values.email) {
+        errors.email = "Required!";
+      }
+      if (!values.password) {
+        errors.password = "Required!";
+      }
+
+      return errors;
+    },
+  });
+  console.log("formik errors", formik.errors);
+
+  return (
+    <div className="form-container">
+      <form className="form-card" onSubmit={formik.handleSubmit}>
+        <label htmlFor="name" className="input-label">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name here!"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          className="input-field"
+        />
+        {formik.errors.name ? (
+          <div className="form-validation">{formik.errors.name} </div>
+        ) : null}
+        <label htmlFor="email" className="input-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email here!"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          className="input-field"
+        />
+        {formik.errors.email ? (
+          <div className="form-validation">{formik.errors.email} </div>
+        ) : null}
+        <label htmlFor="password" className="input-label">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your password here!"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          className="input-field"
+        />
+        {formik.errors.password ? (
+          <div className="form-validation">{formik.errors.password} </div>
+        ) : null}
+        <button type="submit" className="submit-btn">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -4,6 +4,7 @@ const Shimmer = () =>{
     return(
         <>
             <h1 className="Shimmer_rest_heading"></h1>
+            <div className="shimmer-cards">
             <div className="Shimmer_card">
                 <div className="Shimmer_card_item">
                     <div className="Shimmer_card_image"></div>
@@ -12,6 +13,43 @@ const Shimmer = () =>{
                     <p className="Shimmer_card_para shim1"></p>
                     <p className="Shimmer_card_para shim2"></p>
                 </div>
+            </div>
+            <div className="Shimmer_card">
+                <div className="Shimmer_card_item">
+                    <div className="Shimmer_card_image"></div>
+                    <h1 className="Shimmer_card_heading"></h1>
+                    <p className="Shimmer_card_para"></p>
+                    <p className="Shimmer_card_para shim1"></p>
+                    <p className="Shimmer_card_para shim2"></p>
+                </div>
+            </div>
+            <div className="Shimmer_card">
+                <div className="Shimmer_card_item">
+                    <div className="Shimmer_card_image"></div>
+                    <h1 className="Shimmer_card_heading"></h1>
+                    <p className="Shimmer_card_para"></p>
+                    <p className="Shimmer_card_para shim1"></p>
+                    <p className="Shimmer_card_para shim2"></p>
+                </div>
+            </div>
+            <div className="Shimmer_card">
+                <div className="Shimmer_card_item">
+                    <div className="Shimmer_card_image"></div>
+                    <h1 className="Shimmer_card_heading"></h1>
+                    <p className="Shimmer_card_para"></p>
+                    <p className="Shimmer_card_para shim1"></p>
+                    <p className="Shimmer_card_para shim2"></p>
+                </div>
+            </div>
+            <div className="Shimmer_card">
+                <div className="Shimmer_card_item">
+                    <div className="Shimmer_card_image"></div>
+                    <h1 className="Shimmer_card_heading"></h1>
+                    <p className="Shimmer_card_para"></p>
+                    <p className="Shimmer_card_para shim1"></p>
+                    <p className="Shimmer_card_para shim2"></p>
+                </div>
+            </div>
             </div>
         </>  
     )
