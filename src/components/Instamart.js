de44c72bbd8f35fbe0420b825653dfd5dc93fde8
Sanskriti Gupta <21sanskritigupta@gmail.com>
2023-02-02 20:45:49 +0530

    added channges in instamart

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,32 +1,69 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Section=({title, description})=>{
-    const [isVisible, setIsVisible] = useState(false);
-    return (
-        <div className="border-2 border-black p-2 m-2">
-            <h1 className="text-xl font-bold">{title}</h1>
-            {isVisible?<button onClick={()=>{
-                setIsVisible(false)
-            }} className="border-2 border-blue">Hide</button>:<button onClick={()=>{
-                setIsVisible(true)
-            }} className="border-2 border-blue">Show</button>}
-            {/* {isVisible?<p>{description}</p>:<></>} */}
-            {isVisible && <p>{description}</p>}
-        </div>
-    )
-}
+const Section = ({ title, description , isVisible, setIsVisible}) => {
 
+  return (
+    <div className="border-2 border-black p-2 m-2">
+      <h1 className="text-xl font-bold">{title}</h1>
+      {isVisible ? (
+        <button
+          onClick={() => {
+            setIsVisible();
+          }}
+          className="border-2 border-blue"
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            setIsVisible();
+          }}
+          className="border-2 border-blue"
+        >
+          Show
+        </button>
+      )}
+      {/* {isVisible?<p>{description}</p>:<></>} */}
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
+};
 
+const Instamart = () => {
+  const [sectionVisible, setSectionVisible] = useState("about");
+  return (
+    <div className="p-2 m-2">
+      <h1 className="text-3xl font-bold">Instamart</h1>
+      <Section
+        title={"About Instamart"}
+        description={
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque fugit placeat quasi cum facere iusto nam dolor quae. Suscipit, architecto"
+        }
+        isVisible={sectionVisible === "about"}
+        setIsVisible={()=>setSectionVisible(sectionVisible==="about"?"":"about")}
 
-const Instamart = () =>{
-    return(
-        <div className="p-2 m-2">
-            <h1 className="text-3xl font-bold">Instamart</h1>
-            <Section title={"About Instamart"} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque fugit placeat quasi cum facere iusto nam dolor quae. Suscipit, architecto"} />
-            <Section title={"Contact Instamart"} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque fugit placeat quasi cum facere iusto nam dolor quae. Suscipit, architecto"} />
+      />
+      <Section
+        title={"Contact Instamart"}
+        description={
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque fugit placeat quasi cum facere iusto nam dolor quae. Suscipit, architecto"
+        }
+        isVisible={sectionVisible==="contact"}
+        setIsVisible={()=>setSectionVisible(sectionVisible==="contact"?"":"contacts")}
 
-        </div>
-    )
-}
+      />
+      <Section
+        title={"Careers Instamart"}
+        description={
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque fugit placeat quasi cum facere iusto nam dolor quae. Suscipit, architecto"
+        }
+        isVisible={sectionVisible==="careers"}
+        setIsVisible={()=>setSectionVisible(sectionVisible==="careers"?"":"careers")}
+
+      />
+    </div>
+  );
+};
 
 export default Instamart;
