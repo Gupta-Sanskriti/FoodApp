f766cfe04f77e0d8ed70f9076b872a70696c871d
Sanskriti Gupta <21sanskritigupta@gmail.com>
2023-02-09 01:09:56 +0530

    export cartSlice

--- src/utils/ReduxSlices/cartSlice.js
+++ src/utils/ReduxSlices/cartSlice.js
@@ -3,7 +3,7 @@ import {createSlice} from "@reduxjs/toolkit";
 const cartSlice = createSlice({
     name:'cart',
     initialState:{
-        items: [],
+        items: ['Banana'],
     },
     //modify my cart using reducer function
     reducers:{          //these reducers are called on dispatch of an action
@@ -13,6 +13,18 @@ const cartSlice = createSlice({
             //action is the data which is coming in.
             state.items.push(action.payload);   // state here is always the previous state // is there is nothing in the state it would be similar to initial state
             // as in when we change data into our cart it will change the current value of the state.
+            console.log(action.payload)
+            //this function does not return anything from here
+        },
+        clearCart:(state, action) =>{
+            state.items = []
+        },
+        removeItem:(state, action) =>{
+            state.items.pop();
         }
     }
 })
+
+//Export actions and reducers
+export const {addItem, clearCart, removeItem} = cartSlice.actions;
+export default cartSlice.reducer; //this will combine all the reducers and make it as a one reducer
